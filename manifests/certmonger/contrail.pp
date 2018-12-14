552dd48a62f0d73860fb1144d7323ce8c6f9723e
alexey-mr <alexey.morlang@gmail.com>
2018-12-14 15:39:07 +0300

    Integration with IDM
    Change-Id: I5e3001ae2935fdc73a5f8d26a1bfa0d02cfdf447
    Partial-Bug: #1805443

---
+++ manifests/certmonger/contrail.pp
@@ -0,0 +1,65 @@
+# Copyright (C) 2018 Juniper Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::certmonger::contrail
+#
+# Request a certificate for the contrail service and do the necessary setup.
+#
+# === Parameters
+#
+# [*hostname*]
+#   The hostname of the node. this will be set in the CN of the certificate.
+#
+# [*service_certificate*]
+#   The path to the certificate that will be used for TLS in this service.
+#
+# [*service_key*]
+#   The path to the key that will be used for TLS in this service.
+#
+# [*certmonger_ca*]
+#   (Optional) The CA that certmonger will use to generate the certificates.
+#   Defaults to hiera('contrail_certmonger_ca', 'IPA').
+#
+# [*postsave_cmd*]
+#   (Optional) Specifies the command to execute after requesting a certificate.
+#   Defaults to undef
+#
+# [*principal*]
+#   (Optional) The service principal that is set for contrail in kerberos.
+#   Defaults to undef
+#
+class tripleo::certmonger::contrail (
+  $hostname,
+  $service_certificate,
+  $service_key,
+  $certmonger_ca = hiera('contrail_certmonger_ca', 'IPA'),
+  $dnsnames      = $hostname,
+  $postsave_cmd  = undef,
+  $principal     = undef,
+) {
+  include ::certmonger
+  certmonger_certificate { 'contrail' :
+    ensure       => 'present',
+    certfile     => $service_certificate,
+    keyfile      => $service_key,
+    hostname     => $hostname,
+    dnsname      => $dnsnames,
+    principal    => $principal,
+    postsave_cmd => $postsave_cmd,
+    ca           => $certmonger_ca,
+    wait         => true,
+    tag          => 'contrail-cert',
+    require      => Class['::certmonger'],
+  }
+}
