49f77887a57561eb3f94594e54ddeb475bfa5775
alexey-mr <alexey.morlang@gmail.com>
2019-02-25 22:52:33 +0300

    Use paunch for compute during issu
    Change-Id: Ia950fc03a94159054e280f48e7a429b9e9d8cce3
    Partial-Bug: #1801956

--- manifests/network/contrail/issu_compute.pp
+++ manifests/network/contrail/issu_compute.pp
@@ -13,7 +13,6 @@ class tripleo::network::contrail::issu_compute(
   $admin_password               = hiera('contrail::admin_password'),
   $api_server                   = hiera('contrail_config_vip', hiera('internal_api_virtual_ip')),
   $api_port                     = 8082,
-  $internal_api_ssl             = hiera('contrail_internal_api_ssl', false),
   $container_registry           = hiera('contrail_issu_container_registry'),
   $container_tag                = hiera('contrail_issu_container_tag'),
   $old_control_servers          = hiera('contrail::vrouter::control_node_ips', hiera('contrail_control_node_ips')),
@@ -21,6 +20,8 @@ class tripleo::network::contrail::issu_compute(
   $ibgp_auto_mesh               = true,
   $issu_ips                     = hiera('contrail_issu_node_ips'),
   $is_dpdk                      = hiera('contrail::vrouter::is_dpdk',false),
+  $is_tsn                       = hiera('contrail::vrouter::is_tsn',false),
+  $sriov_on                     = hiera('contrail::vrouter::sriov_on', false),
   $keystone_project_domain_name = hiera('contrail::keystone_project_domain_name','Default'),
   $keystone_region              = hiera('contrail::keystone_region','regionOne'),
   $keystone_user_domain_name    = hiera('contrail::keystone_user_domain_name','Default'),
@@ -28,8 +29,15 @@ class tripleo::network::contrail::issu_compute(
   $metadata_host_ip             = hiera('internal_api_virtual_ip'),
   $router_asn                   = hiera('contrail::control::asn'),
   $vrouter_gateway              = hiera('contrail::vrouter::gateway'),
-  $ssl_enabled                  = hiera('contrail_ssl_enabled', false),
   $vhost_user_mode              = hiera('contrail::vrouter::vhost_user_mode', ''),
+  $ssl_enabled                  = hiera('contrail_ssl_enabled', false),
+  $internal_api_ssl             = hiera('contrail_internal_api_ssl', false),
+  $ca_file                      = hiera('contrail::ca_cert_file', undef),
+  $ca_cert                      = hiera('contrail::ca_cert', undef),
+  $ca_key_file                  = hiera('contrail::ca_key_file', undef),
+  $ca_key                       = hiera('contrail::ca_key', undef),
+  $key_file                     = hiera('contrail::service_key_file', undef),
+  $cert_file                    = hiera('contrail::service_cert_file', undef),
 ) {
 
   File {
@@ -49,7 +57,7 @@ class tripleo::network::contrail::issu_compute(
   }
 
   $old_control_servers_list_space = join($old_control_servers, ' ')
-
+  
   $issu_ips_list_space = join($issu_ips, ' ')
   $issu_ips_list_comma = join($issu_ips, ',')
 
@@ -57,6 +65,7 @@ class tripleo::network::contrail::issu_compute(
     "$::hostname" => "$host_ip",
   }
 
+  $tripleo_cfg_dir = '/var/lib/tripleo-config'
   $instances_yaml_file_name="instances-${::hostname}.yaml"
   $issu_dir='/etc/contrail/issu'
   exec { 'set selinux to permissive' :
@@ -70,14 +79,43 @@ class tripleo::network::contrail::issu_compute(
   file { $issu_dir :
     ensure  => directory,
   } ->
-  file { "${issu_dir}/${instances_yaml_file_name}" :
-    ensure  => file,
-    content => template('tripleo/contrail/instances_common.yaml.erb', 'tripleo/contrail/instances_computes.yaml.erb'),
-    mode    => 644,
-  } ->
   file { "${issu_dir}/issu_compute_deploy.sh" :
     ensure  => file,
     content => template('tripleo/contrail/issu_compute_deploy.sh.erb'),
     mode    => 755,
+  } ->
+  file { $tripleo_cfg_dir :
+    ensure  => directory,
+  } ->
+  file { '/var/log/containers' :
+    ensure  => directory,
+  } ->
+  file { "${tripleo_cfg_dir}/hashed-docker-container-startup-config-step_1.json" :
+    ensure  => file,
+    content => template("tripleo/contrail/docker_step1_config.json.erb"),
+    mode    => 600,
+  } ->
+  file { "${tripleo_cfg_dir}/hashed-docker-container-startup-config-step_2.json" :
+    ensure  => file,
+    content => template("tripleo/contrail/docker_step2_config.json.erb"),
+    mode    => 600,
+  } ->
+  file { "${tripleo_cfg_dir}/hashed-docker-container-startup-config-step_3.json" :
+    ensure  => file,
+    content => template("tripleo/contrail/docker_step3_config.json.erb"),
+    mode    => 600,
+  } ->
+  file { "${tripleo_cfg_dir}/hashed-docker-container-startup-config-step_5.json" :
+    ensure  => file,
+    content => template("tripleo/contrail/docker_step5_config.json.erb"),
+    mode    => 600,
+  } ->
+  file { "/etc/contrail/common_contrail.env" :
+    ensure  => file,
+    content => template("tripleo/contrail/common_contrail.env.erb"),
+  } ->
+  file { "/etc/contrail/common_vrouter.env" :
+    ensure  => file,
+    content => template("tripleo/contrail/common_vrouter.env.erb"),
   }
 }
