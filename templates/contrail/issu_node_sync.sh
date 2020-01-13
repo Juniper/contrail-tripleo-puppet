9da4fa730ab06f0591c3775b6614204539fd9aac
alexey-mr <alexey.morlang@gmail.com>
2020-01-13 16:41:55 +0300

    Removed -i option fro docker run
    - this is for ISSU automation.
    
    Change-Id: Ibb33c0f21d985c610475b654ff04d7304abf5b5f
    partial-jira-bug: CEM-11506

--- templates/contrail/issu_node_sync.sh
+++ templates/contrail/issu_node_sync.sh
@@ -40,7 +40,7 @@ chown -R root:root ~/.ssh
 EOF
 chmod +x ${working_dir}/entrypoint.sh
 
-sudo docker run --rm -it --network host \
+sudo docker run --rm -t --network host \
   -v "$working_dir":"$working_dir" \
   -v "$host_ssh_dir":"$working_dir/ssh":ro \
   --entrypoint ${working_dir}/entrypoint.sh \
@@ -62,7 +62,7 @@ chown -R root:root ~/.ssh
 EOF
 chmod +x ${working_dir}/entrypoint.sh
 
-sudo docker run --rm --detach -it --network host --name issu-run-sync \
+sudo docker run --rm --detach --network host --name issu-run-sync \
   -v "$working_dir":"$working_dir" \
   -v "$host_ssh_dir":"$working_dir/ssh":ro \
   --entrypoint ${working_dir}/entrypoint.sh \
